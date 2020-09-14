0cf07dd67a74ead8a9c1bd46874e8960d78ffe21
tundeiness <tundeiness@yahoo.co.uk>
2020-09-14 21:55:13 +0100

    implement insert for linkedlist

---
+++ Pointers/sortColors.js
@@ -0,0 +1,12 @@
+const swap = (arr = [a, b]) =>{
+  return [arr[1], arr[0]]
+}
+
+const sortColors = (arr) => {
+
+}
+
+console.log(swap([2,3]))
+
+
+arr = [2,0,2,1,1,0]
--- linked.rb
+++ linked.rb
@@ -1,15 +1,22 @@
 class Node
   attr_accessor :data, :next_pointer
-  def initialize(data, next_pointer = nil)
+  def initialize(data)
     @data = data
-    @next_pointer = next_pointer
+    @next_pointer = nil
   end
 end
 
 class Linked
+
+  attr_reader :head, :length
+  def initialize
+    @head = nil
+  end
+
   def append(data)
     if @head.nil?
       @head = Node.new(data)
+      @length = 0
     end
 
     current = @head
@@ -18,30 +25,69 @@ class Linked
     end
 
     current.next_pointer = Node.new(data)
+    @length += 1
   end
 
-  def prepend(data)
-    node = Node.new(data)
+
+  def pre(data)
+    current = Node.new(data)
     if @head.nil?
+      @head = current
+    else
+      current.next_pointer = @head
+    end
+    @head = current
+  end
+
+
+  def insert(position, value)
+    # @length = 0
+
+    if @head.nil?
+      current = Node.new(value)
+      @head = current
+    end
+
+
+    if position.zero?
+      node = Node.new(value, @head)
       @head = node
     else
-      node.next_pointer = @head
+      current = @head
+      prev = @head
+      idx = position - 1
+      idx.times do
+        prev = current.next_pointer
+      end
+
+      position.times do
+        current = current.next_pointer
+      end
+      new_node = Node.new(value)
+      after_current = prev.next_pointer
+      prev.next_pointer = new_node
+      new_node.next_pointer = after_current
+
     end
-    @head = node
+    # @length += 1
   end
 end
 
 link = Linked.new
 
 link.append(6)
-link.append(10)
+# link.append(10)
 # link.append(5)
 # link.append(1)
 # link.append(21)
 link.append(14)
-link.prepend('c')
-link.prepend('E')
-link.prepend('a')
+link.append(4)
+# link.pre('c')
+# link.pre('E')
+# link.pre('a')
+# link.pre(100)
+link.insert(3, 'y')
+link.insert(1, 'frodo')
 
 p link
 
